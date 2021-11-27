0b3fe64121545fd50e923f2b06b045e737860f63
taiprovip3 <39458837+taiprovip3@users.noreply.github.com>
2021-11-27 13:11:15 +0700

    Add files via upload

---
+++ data/icon/dataSQL.sql
@@ -0,0 +1,3 @@
+insert into NhaCungCap values ('NCC1',N'Công ty saga',N'Mỹ','0338188506');
+insert into NhaCungCap values ('NCC2',N'Cửa hàng Pencial',N'Pháp','0338188507');
+insert into NhaCungCap values ('NCC3',N'Công ty alten',N'Anh','0338188508');
